613cc9677322b9bcf490bc5c93f47d05e40bb161
jdrueckert <jd.rueckert@googlemail.com>
2021-02-14 21:08:34 +0100

    feat(JOML): migrate behavior tutorial (#9)

--- src/main/java/org/terasology/minion/move/SetTargetToNearbyBlockAwayFromPlayerAction.java
+++ src/main/java/org/terasology/minion/move/SetTargetToNearbyBlockAwayFromPlayerAction.java
@@ -1,21 +1,11 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.minion.move;
 
 import com.google.common.collect.Lists;
+import org.joml.RoundingMode;
+import org.joml.Vector3f;
+import org.joml.Vector3i;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.logic.behavior.BehaviorAction;
@@ -23,7 +13,6 @@ import org.terasology.logic.behavior.core.Actor;
 import org.terasology.logic.behavior.core.BaseAction;
 import org.terasology.logic.behavior.core.BehaviorState;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.minion.component.FleeComponent;
 import org.terasology.navgraph.WalkableBlock;
 import org.terasology.pathfinding.componentSystem.PathfinderSystem;
@@ -44,50 +33,49 @@ public class SetTargetToNearbyBlockAwayFromPlayerAction extends BaseAction {
     private static final Logger logger = LoggerFactory.getLogger(SetTargetToNearbyBlockAwayFromPlayerAction.class);
 
 
-
-        private static final int RANDOM_BLOCK_ITERATIONS = 10;
-        private Random random = new Random();
-
+    private static final int RANDOM_BLOCK_ITERATIONS = 10;
+    private Random random = new Random();
 
 
-        @Override
-        public BehaviorState modify(Actor actor, BehaviorState state) {
-            MinionMoveComponent moveComponent = actor.getComponent(MinionMoveComponent.class);
-            if (moveComponent.currentBlock != null) {
-                WalkableBlock target = findRandomNearbyBlockAwayFromPlayer(moveComponent.currentBlock, actor);
-                moveComponent.target = target.getBlockPosition().toVector3f();
-                actor.save(moveComponent);
-            } else {
-                return BehaviorState.FAILURE;
-            }
-            return BehaviorState.SUCCESS;
+    @Override
+    public BehaviorState modify(Actor actor, BehaviorState state) {
+        MinionMoveComponent moveComponent = actor.getComponent(MinionMoveComponent.class);
+        if (moveComponent.currentBlock != null) {
+            WalkableBlock target = findRandomNearbyBlockAwayFromPlayer(moveComponent.currentBlock, actor);
+            moveComponent.target = new Vector3f(target.getBlockPosition());
+            actor.save(moveComponent);
+        } else {
+            return BehaviorState.FAILURE;
         }
-
-        private WalkableBlock findRandomNearbyBlockAwayFromPlayer(WalkableBlock startBlock, Actor actor) {
-            WalkableBlock currentBlock = startBlock;
-            Vector3i playerPosition = new Vector3i(actor.getComponent(FleeComponent.class).instigator.getComponent(LocationComponent.class).getWorldPosition());
-            for (int i = 0; i < RANDOM_BLOCK_ITERATIONS; i++) {
-                WalkableBlock[] neighbors = currentBlock.neighbors;
-                List<WalkableBlock> existingNeighbors = Lists.newArrayList();
-                for (WalkableBlock neighbor : neighbors) {
-                    if (neighbor != null) {
-                        existingNeighbors.add(neighbor);
-                    }
-                }
-                if (existingNeighbors.size() > 0) {
-                    // Sorting the list of neighboring blocks based on distance from player (farthest first)
-                    existingNeighbors.sort((one, two) -> {
-                        double a = one.getBlockPosition().distanceSquared(playerPosition);
-                        double b = two.getBlockPosition().distanceSquared(playerPosition);
-                        return a > b ? -1
-                                : a < b ? 1
-                                : 0;
-                    });
-                    // Select any of the first 4 neighboring blocks to make path random and not linear
-                    currentBlock = existingNeighbors.get(random.nextInt(min(4, existingNeighbors.size())));
-                }
-            }
-            return currentBlock;
-        }
-
+        return BehaviorState.SUCCESS;
     }
+
+    private WalkableBlock findRandomNearbyBlockAwayFromPlayer(WalkableBlock startBlock, Actor actor) {
+        WalkableBlock currentBlock = startBlock;
+        Vector3i playerPosition =
+                new Vector3i(actor.getComponent(FleeComponent.class).instigator.getComponent(LocationComponent.class).getWorldPosition(new Vector3f()), RoundingMode.FLOOR);
+        for (int i = 0; i < RANDOM_BLOCK_ITERATIONS; i++) {
+            WalkableBlock[] neighbors = currentBlock.neighbors;
+            List<WalkableBlock> existingNeighbors = Lists.newArrayList();
+            for (WalkableBlock neighbor : neighbors) {
+                if (neighbor != null) {
+                    existingNeighbors.add(neighbor);
+                }
+            }
+            if (existingNeighbors.size() > 0) {
+                // Sorting the list of neighboring blocks based on distance from player (farthest first)
+                existingNeighbors.sort((one, two) -> {
+                    double a = one.getBlockPosition().distanceSquared(playerPosition);
+                    double b = two.getBlockPosition().distanceSquared(playerPosition);
+                    return a > b ? -1
+                            : a < b ? 1
+                            : 0;
+                });
+                // Select any of the first 4 neighboring blocks to make path random and not linear
+                currentBlock = existingNeighbors.get(random.nextInt(min(4, existingNeighbors.size())));
+            }
+        }
+        return currentBlock;
+    }
+
+}
