67aef6ac0ea03314efb0cd8bfeb46cc7f56f2cfe
Nail Khanipov <foxflameless@gmail.com>
2021-09-03 01:06:55 +0300

    chore: Update tutorials to lastest engine (#10)
    * chore(tutorial): migrate tutorial modules with engine package movement
    
    * chore(tutorial): migrate components to Gestaltv7
    
    * style: update copyright and remove unused imports
    
    * style: fix remaining checkstyle warnings
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/minion/component/FleeComponent.java
+++ src/main/java/org/terasology/minion/component/FleeComponent.java
@@ -1,30 +1,23 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.minion.component;
 
-import org.terasology.entitySystem.Component;
-import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * This component is used by the FleeOnHit and FleeInProximity module to allow an
  * NPC to exhibit the Flee behavior
  */
-public class FleeComponent implements Component {
+public class FleeComponent implements Component<FleeComponent> {
     /* Minimum distance from instigator after which the NPC will stop 'flee'ing */
     public float minDistance = 10f;
     /* Entity to run away from */
     public EntityRef instigator;
+
+    @Override
+    public void copyFrom(FleeComponent other) {
+        this.minDistance = other.minDistance;
+        this.instigator = other.instigator;
+    }
 }
--- src/main/java/org/terasology/minion/system/TutorialBehaviorsEventSystem.java
+++ src/main/java/org/terasology/minion/system/TutorialBehaviorsEventSystem.java
@@ -1,29 +1,16 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.minion.system;
 
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterMode;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.logic.characters.CharacterMovementComponent;
-import org.terasology.logic.health.event.OnDamagedEvent;
-import org.terasology.minion.component.FleeOnHitComponent;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterMode;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.logic.characters.CharacterMovementComponent;
 import org.terasology.minion.component.FleeComponent;
+import org.terasology.minion.component.FleeOnHitComponent;
+import org.terasology.module.health.events.OnDamagedEvent;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class TutorialBehaviorsEventSystem extends BaseComponentSystem {
